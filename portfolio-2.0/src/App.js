ebd4ffcd6bc69a383b9bd56780b39fec9cfad118
RyzaT <ryantaylor50@hotmail.com>
2023-03-09 13:16:06 +0000

    Navbar added

--- portfolio-2.0/src/App.js
+++ portfolio-2.0/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Navbar />
+      <Hero/>
     </div>
   );
 }
---
+++ portfolio-2.0/src/Components/Hero/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styles from './index.module.css'
+
+const Hero = () => {
+    return (
+        <div className={styles.container}>
+
+            <div className={styles.hero}>
+                <div className={styles.mainContainer}>
+                    <div className={styles.text_container}>
+                        <h3>I'm</h3>
+                        <h1>Ryan Taylor</h1>
+                        <div>Web Designer, Web Developer, Front End Developer</div>
+                    </div>
+                    <div className={styles.profile}>
+                        <img className={styles.image} src="portfolio-2.0/public/cartoon.jpg" alt="me"></img>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
