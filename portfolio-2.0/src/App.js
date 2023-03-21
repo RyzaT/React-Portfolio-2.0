b52183e80b4b7595a62998e023ae0862a47f2f8a
RyzaT <ryantaylor50@hotmail.com>
2023-03-21 08:42:05 +0000

    Project section base

--- portfolio-2.0/src/App.js
+++ portfolio-2.0/src/App.js
@@ -9,7 +9,7 @@ import Title from './Components/Title';
 import Wrapper from './Components/Wrapper';
 import props from './Components/Projects/Project.json';
 import ProjectCard from './Components/Projects/Project';
-import Projects from "./Components/Projects/Project";
+import Projects from "./Components/Projects/Projects";
 import Header from './Components/Banner/Header';
 import styled from "styled-components";
 import ProfComponent from './Components/Banner/ProfComponent';
@@ -27,10 +27,13 @@ function App() {
     <Router>
       <Container>
         <Banner>
-        <Header />
-        <ProfComponent />
+          <Header />
+          <ProfComponent />
         </Banner>
         <Services />
+        <LightColor>
+          <Projects />
+        </LightColor>
       </Container>
       <Navbar />
       <Routes>
@@ -61,4 +64,13 @@ export default App;
 const Container = styled.div``
 const Banner = styled.div`
   background: linear-gradient(159deg, rgb(45, 45, 58) 0%, rgb(43, 43, 53) 100%);
-  `
+  height: 100vh;
+  @media (max-width: 640px) {
+    height: 100%;
+    padding-bottom: 2rem;
+  }
+`;
+
+const LightColor = styled.div`
+  background: linear-gradient(159deg, rgb(45, 45, 58) 0%, rgb(43, 43, 53) 100%);
+`;
